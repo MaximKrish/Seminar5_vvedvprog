d513b2f7b37f9b6ab203e867aa591cfcb07af8d2
Maxim <m.krishnevskiy@kmg.kz>
2022-07-07 14:58:30 +0600

    Seminar 5

---
+++ Zadacha31/Program.cs
@@ -0,0 +1,68 @@
+﻿// void factorial(int[]count){
+//     int count =1;
+//     for (int i = 0; i < = n; i++)
+//     count = count*i;
+//     {
+//         count = count*i;
+//     }
+// }
+
+
+
+
+int [] array = new int[12];
+int[] sum = new int[2];
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(-9, 9); //8 длина массива
+        index++;
+    }
+}
+
+void PrintArray(int[]col){
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+    for (int i = 0; i < col.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+        if (array[i] > 0) 
+            sum[0] = sum[0] + array[i];
+        else 
+            sum[1] = sum[1] + array[i];
+    }
+   
+}
+FillArray(array);
+PrintArray(array);
+
+  
+Console.WriteLine("Суммы положительных цифр = {0} и отр. = {1}", sum[0], sum[1]);
+
+
+
+
+
+// int[] array = new int[12];
+// int[] sum = new int[2];
+// void FillArray(int[] collection, int[] sum)
+// {
+//     for (int i = 0; i < collection.Length; i++)
+//     {
+//         array[i] = new Random().Next(-9, 10);
+//         if (array[i] > 0) 
+//             sum[0] = sum[0] + array[i];
+//         else 
+//             sum[1] = sum[1] + array[i];
+//     }
+// }
+// FillArray(array, sum);
+// Console.WriteLine("Суммы положительных цифр = {0} и отр. = {1}", sum[0], sum[1]);
---
+++ Zadacha37/Program.cs
@@ -0,0 +1,13 @@
+﻿int n;
+Console.WriteLine("Напишите кол-во чисел: ");
+n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Console.WriteLine("Напишите числа: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i <= array.Length / 2; i++)
+{
+    Console.WriteLine(array[i] * array[array.Length - i - 1]);
+}
